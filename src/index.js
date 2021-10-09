cf04f2c64fcfa1f3878c2b7ee05bee90798a93a7
Khusniddin <xusainovx@gmail.com>
2021-10-09 23:47:10 +0500

    add redux, implemented userReducer & userAction to header and app components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 import App from './App';
 import './index.css';
 
+const Application = () => (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+)
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Application/>,
   document.getElementById('root')
 );
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import {createStore, applyMiddleware} from "redux";
+import {createLogger} from "redux-logger/src";
+
+import rootReducer from './root-reducer';
+
+const logger = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+export default store;
