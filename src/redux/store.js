62b8fc249e220c26760f2acfc0ece82701d67426
lollapaloooza <xusainovx@gmail.com>
2021-10-27 22:23:32 +0500

    removing redux logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV === 'development') {
     composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 
-export const store = createStore(rootReducer, composeEnhancers());
+export const store = createStore(rootReducer, composeEnhancers);
 export const persistor = persistStore(store);
 
 // eslint-disable-next-line import/no-anonymous-default-export
